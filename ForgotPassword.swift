6106f61ee173f5ddfaa6680890f5b39e149ca0ea
srraghunath <srraghu2005@gmail.com>
2025-11-04 08:07:37 +0530

    implement fotgot password ui

---
+++ ForgotPassword.swift
@@ -0,0 +1,29 @@
+//
+//  ForgotPassword.swift
+//  FitClient
+//
+//  Created by admin6 on 04/11/25.
+//
+
+import UIKit
+
+class ForgotPassword: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
