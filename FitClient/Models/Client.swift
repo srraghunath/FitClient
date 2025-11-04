9435530e1df99a386ab21b200e78be3567eec22a
srraghunath <srrgahu2005@gmail.com>
2025-11-04 23:45:10 +0530

    implement and connect trainer dahsboard ui

---
+++ FitClient/Models/Client.swift
@@ -0,0 +1,8 @@
+//
+//  Client.swift
+//  FitClient
+//
+//  Created by admin8 on 04/11/25.
+//
+
+import Foundation
---
+++ FitClient/Models/Trainer.swift
@@ -0,0 +1,8 @@
+//
+//  Trainer.swift
+//  FitClient
+//
+//  Created by admin8 on 04/11/25.
+//
+
+import Foundation
