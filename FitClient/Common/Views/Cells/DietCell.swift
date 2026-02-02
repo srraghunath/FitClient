c7404ed23626105ae4fa4f1580a9527358c8c56c
vickashnarayan07 <veekanarry@gmail.com>
2026-02-02 15:33:56 +0530

    STEPPER FIXED

--- FitClient/Common/Views/Cells/DietCell.swift
+++ FitClient/Common/Views/Cells/DietCell.swift
@@ -68,8 +68,8 @@ class DietCell: UITableViewCell {
         
         nameLabel.text = diet.name
         
-        // Format nutrition info: "150g | P: 25g | C: 40g | F: 10g | 350 cal"
-        nutritionLabel.text = "100 gn | P: \(String(format: "%.1f", diet.protein))g | C: \(String(format: "%.1f", diet.carbs))g | F: \(String(format: "%.1f", diet.fat))g | \(diet.calories) cal"
+    // Format nutrition info: "<grams> g | P: 25g | C: 40g | F: 10g | 350 cal"
+    nutritionLabel.text = "\(diet.grams) g | P: \(String(format: "%.1f", diet.protein))g | C: \(String(format: "%.1f", diet.carbs))g | F: \(String(format: "%.1f", diet.fat))g | \(diet.calories) cal"
         
         // Configure selection state
         if diet.isSelected {
